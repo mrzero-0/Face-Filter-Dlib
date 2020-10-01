0f970d8e1d14fe60bba1554a6189fef1be0aeff0
Devesh <deveshvmwteam@gmail.com>
2020-10-01 18:01:44 +0530

    Face Detection Added

--- Face Landmarks.py
+++ Face Landmarks.py
@@ -1,3 +1,20 @@
 import cv2
 import numpy as np
 import dlib
+
+img = cv2.imread("myface.jpg")
+img = cv2.resize(img, (0, 0), None, 0.8, 0.8)
+
+imgOriginal = img.copy()
+
+detector = dlib.get_frontal_face_detector()
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+faces = detector(imgGray)
+
+for face in faces:
+    x1, y1 = face.left(), face.top()
+    x2, y2 = face.right(), face.bottom()
+    imgOriginal = cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+cv2.imshow("Original", imgOriginal)
+cv2.waitKey(0)
